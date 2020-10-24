4064308bb91d67088acbe08fd4df877f30c3fa32
metroid-vania <sideroll@gmail.com>
2020-10-24 20:36:56 +0900

    コンタクトページ (contact.html) を追加

--- js/script.js
+++ js/script.js
@@ -33,22 +33,6 @@ $(function () {
   });
 });
 
-/* アコーディオン */
-$(function () {
-  $('.js-accordion__title').on('click', function () {
-    $(this).next().slideToggle(200);
-    $(this).toggleClass('is-open', 200);
-  });
-});
-
-/* nav メニュー */
-$(function () {
-  $('.js-nav__link').on('click', function () {
-    $('.js-nav__link').removeClass('current');
-    $(this).addClass('current');
-  });
-});
-
 /* ヘッダーカラーの変更 */
 $(window).on('scroll', function () {
   if ($(this).scrollTop() > $('.mv').height() - $('.header').height()) {
@@ -57,30 +41,6 @@ $(window).on('scroll', function () {
     $('.header').css('background-color', 'transparent');
   }
 });
-
-/* wow js */
-new WOW().init();
-
-
-/* modaal */
-$('.modal').modaal();
-
-/* mailform (autoKana js) */
-(function ($) {
-  $.fn.autoKana('.form__name_1', '.form__read_1', {
-    katakana: false
-  });
-  $.fn.autoKana('.form__name_2', '.form__read_2', {
-    katakana: false
-  });
-})(jQuery);
-
-/* mailform (datetimepicker js) */
-(function ($) {
-  $('.form__schedule').datetimepicker({
-    lang: 'ja'
-  });
-})(jQuery);
 
 /* Safari(iOS) 対策 */
 const setFillHeight = () => {
